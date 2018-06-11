fa91a2095f37377fd83c7323340a081979495d16
Sitwat <sitwat.nadeem1@gmail.com>
2018-06-10 21:04:49 -0400

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,15 +5,18 @@ var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unAnswered = 0;
 
- 
-newQuestion(function(){
 
-});
 
-var questions = ["Clouds are caused by what?", "In cricket what is an ODI?"]
+
+var questions = ["Clouds are caused by what?", 
+                "In cricket what is an ODI?",
+                "In which year of First World War Germany declared war on Russia and France?"]
+
 var choices = [["Evaporation","Water vapour condensing","Melting ice crystals","Transpiration"],
-                ["Off Drive Initiative", "One Day Innings", "One Day International", "Over Delivered Illegally"]]
-var answers = ["Water vapour condensing", "One Day International"]
+               ["Off Drive Initiative", "One Day Innings", "One Day International", "Over Delivered Illegally"]
+               ["1914","1915", "1916", "1917"]]
+                
+var answers = ["Water vapour condensing", "One Day International","1914"]
 	
 	
 
@@ -22,14 +25,11 @@ for ( var i = 0; i < questions.length; i++){
     setInterval(function() {
     $("#timesUp").html("Time Out")
       }, 30000);
-      setInterval(function(){
-        questionCounter++;
-        newQuestion();
-      }, 30000);
-}
+      
+      }
 
 for ( var x = 0; x < choices.length; x++){
-    
+
 }
 
 });
---
+++ assets/javascript/app1.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+   
+    var triviaQuestions = [{
+        question: "All Life begins with a single _____________",
+        answerList: ["tissue", "cell", "microbe", "organ"],
+        answer: 1
+    },{
+        question: "Skin, ears, and kidneys are examples of _____________",
+        answerList: ["cells", "organ systems", "organs", "tissues"],
+        answer: 2
+    },{
+        question: "One important function of bones is to produce  _____________",
+        answerList: ["blood cells", "tendons", "cartilage", "ligaments"],
+        answer: 0
+    },{
+        question: "Digestion begins in the   _____________",
+        answerList: ["large intestine", "small intestine", "mouth", "stomach"],
+        answer: 2
+    },{
+        question: "The human heart has _____________ chambers (rooms).",
+        answerList: ["Five", "Four", "Two", "Three"],
+        answer: 1
+    },{
+        question: "The main organ in the respiratory system is the _____________.",
+        answerList: ["Trachea", "Diaphragm", "Lung", "Bronchi"],
+        answer: 2
+    },{
+        question: " The central nervous system includes:",
+        answerList: ["Spinal Cord", "Brain & Spinal Cord", "Nerves", "Brain"],
+        answer: 1
+    },{
+        question: "What is the term which describes the joining together of a sperm and an egg?",
+        answerList: ["Fertilization", "Ovulation", "Reproduction", "Duplication"],
+        answer: 0
+    },{
+        question: "The primary function of the system is to support and protect the body",
+        answerList: ["Digestive", "Muscular", "Skeletal", "Respiratory"],
+        answer: 2
+    },{
+        question: "The primary function of this system is to break down food into usable form and to remove waste products",
+        answerList: ["Muscular", "Respiratory","Skeletal", "Digestive"],
+        answer: 3
+    }]
+});
