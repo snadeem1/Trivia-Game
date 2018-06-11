a45822e29764de6410f8a62d31c6b421773ab9b0
Sitwat <sitwat.nadeem1@gmail.com>
2018-06-10 23:32:38 -0400

    final js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
    
+    var currentQuestion; 
+    var correctAnswer; 
+    var incorrectAnswer;
+    var unanswered;
+    var seconds;
+    var time; 
+    var answered; 
+    var userSelect;
+    var messages = {
+        correct: "YAY! You got it right",
+        incorrect: "Ooops! That's wrong",
+        endTime: "Out of time!",
+        finished: "Time to see the Results"
+    }
+    
+    
     var triviaQuestions = [{
         question: "All Life begins with a single _____________",
         answerList: ["tissue", "cell", "microbe", "organ"],
@@ -44,21 +60,6 @@ $(document).ready(function() {
 
 var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10'];
 
-var currentQuestion; 
-var correctAnswer; 
-var incorrectAnswer;
-var unanswered;
-var seconds;
-var time; 
-var answered; 
-var userSelect;
-
-var messages = {
-	correct: "YAY! You got it right",
-	incorrect: "Ooops! That's wrong",
-	endTime: "Out of time!",
-	finished: "Time to see the Results"
-}
 
 $('#startBtn').on('click', function(){
     $(this).hide();
@@ -124,4 +125,48 @@ function showCountdown(){
 	}
 }
 
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	$('#gif').html('<img src = "assets/images/'+ gifArray[currentQuestion] +'.gif" width = "400px">');
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 2000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 2000);
+	}	
+}
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
 });
